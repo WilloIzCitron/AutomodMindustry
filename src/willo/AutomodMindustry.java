2c26aaafc778e1faa8948f4c02a1be89f7fd10c1
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2021-03-29 21:06:43 +0700

    Initial Release

---
+++ src/willo/AutomodMindustry.java
@@ -0,0 +1,20 @@
+package willo;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.net.Administration.*;
+import mindustry.world.blocks.storage.*;
+
+public class AutomodMindustry extends Plugin{
+
+    @Override
+    public void init(){
+    }
+
+
+}
